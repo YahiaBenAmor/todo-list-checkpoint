b1ddde218d8be2db6ef56f27b35005d5ecbf2546
yahia ben amor <yahiabenamor3@gmail.com>
2020-06-11 09:59:08 +0200

    ToDo-List-Checkpoint

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import Todo from "../ToDo/Todo";
+import NewTodoForm from "../NewTdoForm/NewTodoForm";  
+import "./TodoList.css";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const create = newTodo => {
+    console.log(newTodo);
+    setTodos([...todos, newTodo]);
+  };
+
+  const remove = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const update = (id, updtedTask) => {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, task: updtedTask };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const toggleComplete = id => {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const todosList = todos.map(todo => (
+    <Todo
+      toggleComplete={toggleComplete}
+      update={update}
+      remove={remove}
+      key={todo.id}
+      todo={todo}
+    />
+  ));
+
+  return (
+    <div className="TodoList">
+      <h1>
+        Todo List
+      </h1>
+      <ul>{todosList}</ul>
+      <NewTodoForm createTodo={create} />
+    </div>
+  );
+}
+
+export default TodoList;
